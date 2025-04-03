1d6ededf3f6ac712a70a0fa2389c928df531b12a
Pratik Raj <pratikraj220011@gmail.com>
2025-04-03 14:25:23 +0530

    implemented query based search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import SearchForm from '@/components/SearchForm'; 
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
-import { client } from '@/sanity/lib/client';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+import { sanityFetch } from '@/sanity/lib/lives';
+import { SanityLive } from '@/sanity/lib/lives';
 
 export default async function Home({ searchParams }: 
   { searchParams: Promise<{ query?: string }>}) {
     const query = (await searchParams).query;
-
-    const posts = await client.fetch(STARTUPS_QUERY);
+    const params = { search: query || null };
+    const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -36,6 +37,8 @@ export default async function Home({ searchParams }:
         </ul>
 
       </section>
+
+      <SanityLive/>
     </>
   );
 }
