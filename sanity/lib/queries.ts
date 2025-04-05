09bdf0776af36b33e6074e8518a3bbfde610b453
Pratik Raj <pratikraj220011@gmail.com>
2025-04-05 17:52:24 +0530

    solved the issue with query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,31 +1,19 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY = defineQuery(`*[
-  _type == "startup" &&
-  defined(slug.current) &&
-  (
-    !defined($search) ||
-    title match "*$search*" ||
-    category match "*$search*" ||
-    author->name match "*$search*"
-  )
-] | order(_createdAt desc) {
+export const STARTUPS_QUERY =
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
   _id, 
   title, 
   slug,
   _createdAt,
   author -> {
-    _id,
-    name,
-    image,
-    bio
+    _id, name, image, bio
   }, 
   views,
   description,
   category,
-  image
+  image,
 }`);
-
 
 export const STARTUP_BY_ID_QUERY =
   defineQuery(`*[_type == "startup" && _id == $id][0]{
